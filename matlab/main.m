d64e585f15ea96a2d36e01faeb6b0505f063dee4
August Johansson <august.johansson@sintef.no>
2024-01-05 09:04:43 +0100

    First commit

---
+++ matlab/main.m
@@ -0,0 +1,26 @@
+if pyenv().Version == "" % cannot use isempty
+    pyenv('Version', '/usr/bin/python3.10');
+end
+
+disp('Before:')
+disp(py.sys.path)
+modpaths = {fullfile(pwd, 'moduledir'), '/tmp/misc_modules'};
+for k = 1:numel(modpaths)
+    modpath = modpaths{k};
+
+    %[st, res] = system(sprintf('rm -rf %s/__pycache__'));
+
+    if count(py.sys.path, modpath) == 0
+        %py.sys.path.insert(int32(0), modpath); % doesn't work
+        insert(py.sys.path, int32(0), modpath);
+    end
+end
+disp('After:')
+disp(py.sys.path)
+
+modnames = {'mymod', 'somemod'};
+for k = 1:numel(modnames)
+    modname = modnames{k};
+    py.importlib.import_module(modname);
+    py.(modname).run();
+end
